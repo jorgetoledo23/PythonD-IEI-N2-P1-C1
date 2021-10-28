0c0a0a07308967d6c14504533c99fb2a65fc4d90
jorgetoledo23 <jorge.toledo.6@hotmail.com>
2021-10-28 12:20:42 -0300

    Clase 28 Octubre

---
+++ Unidad 2/ConexionMySql.py
@@ -0,0 +1,16 @@
+import mysql.connector
+from mysql.connector import errorcode
+
+try:
+  cnx = mysql.connector.connect(user='root', password='',
+                              host='127.0.0.1',
+                              database='d_eie_p1_c1')
+except mysql.connector.Error as err:
+  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+    print("Something is wrong with your user name or password")
+  elif err.errno == errorcode.ER_BAD_DB_ERROR:
+    print("Database does not exist")
+  else:
+    print(err)
+else:
+    print('Conexion Establecida Correctamente!')
