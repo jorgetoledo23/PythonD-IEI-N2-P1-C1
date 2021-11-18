cfa1057f79f96507163cc444c13a89caf925a7c8
jorgetoledo23 <jorge.toledo.6@hotmail.com>
2021-11-18 12:13:55 -0300

    Clase 18 Nov

--- Unidad 2/ConexionMySql.py
+++ Unidad 2/ConexionMySql.py
@@ -80,3 +80,11 @@ class DAO:
             A = Auto(patente, numero_chasis,color, marca, year, modelo, rut_cliente)
             lista.append(A)
         return lista
+
+#CRUD
+
+#C => Create => Insertar datos en la BD
+#R => Read => Select * from tabla
+
+#U => Update => Actualizar registros
+#D => Delete => Eliminar registros ya almacenados
---
+++ Unidad 2/Ejercicio 1 CRUD tblEquipos.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE EjercicioCrud;
+
+USE EjercicioCrud;
+
+CREATE TABLE tbl_EquiposChilenos (
+	DNI int primary key,
+    Nombre varchar(100),
+    EstadioLocalia varchar(100),
+    CapacidadEstadio int,
+    Ciudad varchar(100)
+)
+
+#Ejercicio: Insertar a traves de un Script de Python al menos un registro 
+#A la tabla tbl_EquiposChilenos
+
+#A traves de Python, Listar todos los equipos almacenados en tbl_EquiposChilenos
+
